96283208bc66f94facccef435c6b93ff7db7911b
Виктория Мушникова <mushnikovavikasuper@gmail.com>
2026-09-10 21:09:02 +0300

    Lab02: char and decimal precision

--- obj/Debug/net10.0/Lab02-Мушникова.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Мушникова.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Мушникова")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f59513b810edf10c3fa4ab6a5928e72b2e4d688")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Мушникова")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Мушникова")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
